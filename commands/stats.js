20bf82e2991b9faa13585fc148b8c7036dbb218a
dhjwst <dhjwst@gmail.com>
2024-06-09 04:35:32 +0300

    Исправление грамматики

--- commands/stats.js
+++ commands/stats.js
@@ -3,7 +3,7 @@ import { getStats } from "./../utils.js";
 
 export async function run (bot, msg, args) {
     getStats(msg.chat.id).then(async stats => {
-        await bot.sendMessage(msg.chat.id, `📄 *Статистика*\n\n🫵 Ваша статистика\nВы запросили рассписаний: ${stats.schedulesReceived}\nРасписаний в избранном: ${stats.favorites}\n\n🤖Статистика бота\nОбработано сообщений: ${msg.message_id}\nВсего запросили расписаний: ${stats.schedulesReceivedTotal}\nПользователей: ${stats.users}`, {parse_mode: "Markdown"});
+        await bot.sendMessage(msg.chat.id, `📄 *Статистика*\n\n🫵 Ваша статистика\nВы запросили расписаний: ${stats.schedulesReceived}\nРасписаний в избранном: ${stats.favorites}\n\n🤖Статистика бота\nОбработано сообщений: ${msg.message_id}\nВсего запросили расписаний: ${stats.schedulesReceivedTotal}\nПользователей: ${stats.users}`, {parse_mode: "Markdown"});
         await bot.deleteMessage(msg.chat.id, msg.message_id)
     })
 }
